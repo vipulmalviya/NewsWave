d3ece5d737b6ec27712ba7c37e83894236b74f22
vipulmalviya <vipulmalviya9987@gmail.com>
2023-08-22 14:44:42 +0530

    Updates

---
+++ precache-manifest.301e705835a1a46da08014ba421201a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7b225093db75cf3737",
+    "url": "/newswave/static/css/main.c0233087.chunk.css"
+  },
+  {
+    "revision": "8e7b225093db75cf3737",
+    "url": "/newswave/static/js/main.8e7b2250.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/newswave/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "ff55685d1e37ef078397",
+    "url": "/newswave/static/js/2.ff55685d.chunk.js"
+  },
+  {
+    "revision": "b37db840299128a28826",
+    "url": "/newswave/static/js/runtime~main.b37db840.js"
+  },
+  {
+    "revision": "af6ada67b250320813b1723e5ae468cc",
+    "url": "/newswave/static/media/Dual.af6ada67.gif"
+  },
+  {
+    "revision": "fdc88ff1a47656c25636ad898997e98f",
+    "url": "/newswave/index.html"
+  }
+];
